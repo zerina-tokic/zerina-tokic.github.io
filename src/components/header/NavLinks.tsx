8bfc7478a23a0e8ca99e32cb5a37acb6a869f57d
zerina-tokic <tokiczerinaa@gmail.com>
2024-05-15 11:08:57 +0200

    create mobile menu

--- src/components/header/NavLinks.tsx
+++ src/components/header/NavLinks.tsx
@@ -1,7 +1,7 @@
 function NavLinks() {
   return (
     <div className="navlinks flex flex-grow items-center justify-center barlow-regular text-[20px] text-[#808080]">
-      <nav className="hidden md:flex gap-x-10">
+      <nav className="hidden lg:flex gap-x-10">
         <a href="/" className="active-link">
           Home
         </a>
